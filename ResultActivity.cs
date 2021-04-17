df0ebc0eba89dabfcd98488cd4d2df63c0ec46d8
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-04-17 21:43:31 +0700

    ใช้ AppCompatActivity เพื่อแสดง app bar

--- ResultActivity.cs
+++ ResultActivity.cs
@@ -1,20 +1,12 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using AndroidX.AppCompat.App;
 
 namespace CalculatorApp
 {
     [Activity(Label = "ResultActivity")]
-    public class ResultActivity : Activity
+    public class ResultActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
